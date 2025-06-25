93fbe8ff9e0cf9e2d7b6ffb39ae343d89387bcea
John Palmer <johnrbpalmer@gmail.com>
2025-06-26 01:02:10 +0200

    made bash script for automation

---
+++ update_weather.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# making sure gdal path is correct
+export PATH=/home/soft/gdal-2.3.2/bin/:$PATH
+export LD_LIBRARY_PATH=/home/soft/gdal-2.3.2/lib/:$LD_LIBRARY_PATH
+
+# starting in project directory
+cd ~/research/realtime-weather-spain
+
+# pull in any pending commits
+git pull origin main
+
+/home/soft/R-4.1.0/bin/R CMD BATCH --no-save --no-restore code/get_latest_data.R logs/get_latest_data.out 
+
+
+# Commit and push the log files from this latest run
+git add --all
+git commit -m 'new data and log files (cluster - automated)'
+git pull origin main
+git push origin main
+
+# run using:
+# qsub -q ceab -pe make 1 -l h_vmem=6G -m bea -M johnrbpalmer@gmail.com ~/research/realtime-weather-spain/update_weather.sh
